1ea154911200d8f937cbd4cb45c78afdc930c381
Mainara <mainara.farias@ccc.ufcg.edu.br>
2018-07-11 11:59:17 -0300

    Adicionei README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Object Detection with preprocessing
+# Detecção de Objetos com diferentes pré-processamentos
+
+Para isntalar as dependências do projeto e executá-lo, utilize os seguintes comandos:
+
+```sh
+# aqui ja ira instalar as dependencias do arquivo requirements.txt
+./install.sh
+```
+
+O script install.sh irá instalar as dependências dentro de um ambiente virtual, então é necessário ativá-lo antes de executar o código:
+
+```sh
+# ativa ambiente virtual
+. .env/bin/activate
+```
+
+Para realizar a detecção é necessário alguns argumentos:
+* --imagem: caminho da imagem de entrada
+* --prototxt: caminho para Caffe 'deploy' prototxt file
+* --model: caminho para o modelo Caffe pre-treinado
+* --confidence (opcional): probabilidade minima para filtrar deteccoes fracas
+
+Exemplo de execução:
+
+```sh
+python object-detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel --imagem imagens/example_01.jpg
+```
 
--- object-detection.py
+++ object-detection.py
@@ -1,78 +1,98 @@
-# package used to aid in the manipulation of lists
+# pacote utilizado para auxiliar na manipulacao de listas
 import numpy as np
 
-# module used to make easy to write user-friendly command-line interfaces
+# pacote utilizado para auxiliar a interface de comando de linha
 import argparse
 
-# package used to aid in the manipulation of images
+# pacote utilizado para auxiliar a manipulacao de imagemns
 import cv2
  
-class ObjectDetector:
+class DetectorObjetos:
 
-    # initialize the list of class labels MobileNet SSD was trained to
-    # detect, then generate a set of bounding box colors for each class
-    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
-    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+    args = None
 
-    # construction of the arguments necessary to perform the detection
-    def getArgs():
+    # construcao dos argumentos necessarios para executar a deteccao
+    def getArgs(self):
+        global args
         argp = argparse.ArgumentParser()
-        argp.add_argument("-i", "--image", required=True, help="path to the image")
-        argp.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
-        argp.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
-        argp.add_argument("-c", "--confidence", type=float, default=0.2, help="minimum probability to filter weak detections")
+        argp.add_argument("-i", "--imagem", required=True, help="caminho para a imagemm de entrada")
+        argp.add_argument("-p", "--prototxt", required=True, help="caminho para Caffe 'deploy' prototxt file")
+        argp.add_argument("-m", "--model", required=True, help="caminho para o modelo Caffe pre-treinado")
+        argp.add_argument("-c", "--confidence", type=float, default=0.2, help="probabilidade minima para filtrar deteccoes fracas")
         args = vars(argp.parse_args())
 
 
-    # load our serialized model from disk
-    def loadModel():
-        print("[INFO] loading model...")
+    # carraga o arquivo do modelo
+    def carregaModelo(self):
+        global args
+        print("[INFO] carregando modelo...")
         net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+        return net
 
-    # load the input image and construct an input blob for the image
-    # by resizing to a fixed 300x300 pixels and then normalizing it
-    # (note: normalization is done via the authors of the MobileNet SSD
-    # implementation)
-    def loadImage():
-        image = cv2.imread(args["image"])
-        (h, w) = image.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+    # carrega a imagem de entrada e constroi um blob de entrada para a imagem
+    # redimencionando a mesma para 300x300 pixels e fazendo a normalizacao
+    # (info: a normalizacao eh feita pelo as autores que implementaram o MobileNet SSD)
+    def carregaImagem(self):
+        global args
+        imagem = cv2.imread(args["imagem"])
+        (h, w) = imagem.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(imagem, (300, 300)), 0.007843, (300, 300), 127.5)
+        return {'imagem':imagem, 'blob':blob, 'w': w, 'h':h}
 
-    # pass the blob through the network and obtain the detections and
-    # predictions
-    def getDetections:
-        print("[INFO] computing object detections...")
+    # passa o blob pela redes neurais e obtem as deteccoes e predicoes
+    def getDeteccoes(self, net, blob):
+        print("[INFO] calculando as deteccoes...")
         net.setInput(blob)
-        detections = net.forward()
+        deteccoes = net.forward()
+        return deteccoes
 
-# loop over the detections
-for i in np.arange(0, detections.shape[2]):
-	# extract the confidence (i.e., probability) associated with the
-	# prediction
-	confidence = detections[0, 0, i, 2]
- 
-	# filter out weak detections by ensuring the `confidence` is
-	# greater than the minimum confidence
-	if confidence > args["confidence"]:
-		# extract the index of the class label from the `detections`,
-		# then compute the (x, y)-coordinates of the bounding box for
-		# the object
-		idx = int(detections[0, 0, i, 1])
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
- 
-		# display the prediction
-		label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-		print("[INFO] {}".format(label))
-		cv2.rectangle(image, (startX, startY), (endX, endY),
-			COLORS[idx], 2)
-		y = startY - 15 if startY - 15 > 15 else startY + 15
-		cv2.putText(image, label, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+    # loop sobre as detccoes
+    def loopSobreDeteccoes(self, deteccoes, imagem, w, h):
 
-# show the output image
-cv2.imshow("Output", image)
-cv2.waitKey(0)
+        # inicializa os labels que o MobileNet SSD foi treinado para
+        # detectar, e entao gerar os bounding boxes de CORES para cada classe
+        CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	    "sofa", "train", "tvmonitor"]
+        CORES = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+        for i in np.arange(0, deteccoes.shape[2]):
+	        # extrai a probabilidade associada com a predicao
+	        confidence = deteccoes[0, 0, i, 2]
+ 
+            # filtra as deteccoes fracas de acordo com o calor de `confidence`
+	        if confidence > args["confidence"]:
+		        # extrai o indice da classe com determinado label em `deteccoes`,
+		        # e calcula as coordenadas (x, y) do bound box criado para o objeto
+		        idx = int(deteccoes[0, 0, i, 1])
+		        box = deteccoes[0, 0, i, 3:7] * np.array([w, h, w, h])
+		        (comecoX, comecoY, fimX, fimY) = box.astype("int")
+		        
+                # exibe a previsao
+		        label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+		        print("[INFO] {}".format(label))
+		        cv2.rectangle(imagem, (comecoX, comecoY), (fimX, fimY),
+			        CORES[idx], 2)
+		        y = comecoY - 15 if comecoY - 15 > 15 else comecoY + 15
+		        cv2.putText(imagem, label, (comecoX, y),
+			        cv2.FONT_HERSHEY_SIMPLEX, 0.5, CORES[idx], 2)
+        return imagem
+
+    # mostra a imagem de saida
+    def mostraImagem(self, imagem):
+        cv2.imshow("Output", imagem)
+        cv2.waitKey(0)
+
+if __name__ == "__main__":
+    od = DetectorObjetos()
+    od.getArgs()
+    net = od.carregaModelo()
+    dic = od.carregaImagem()
+    imagem = dic['imagem']
+    blob = dic['blob']
+    w = dic['w']
+    h = dic['h']
+    deteccoes = od.getDeteccoes(net, blob)
+    imagem2 = od.loopSobreDeteccoes(deteccoes, imagem, w, h)
+    od.mostraImagem(imagem2)
